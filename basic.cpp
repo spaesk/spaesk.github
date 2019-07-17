723fe9f574946b6356f45786922031f95389275d
spaesk <37662056+spaesk@users.noreply.github.com>
2019-07-17 10:32:23 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define lli long long int
+
+using namespace std;
+
+int main()
+{
+    lli n;
+    cin>>n;
+    cout<<"Wow, you entered "<<n<<endl;
+    cout<<"But you know what thats what i thought, lol"<<endl;
+}
